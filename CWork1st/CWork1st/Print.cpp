ed27e8f1909e3b1a8f9a79af03b66f0da2f56ab6
MikhailGorbach <michagorbach57@gmail.com>
2021-10-04 15:38:21 +0300

    print move list

--- CWork1st/CWork1st/Print.cpp
+++ CWork1st/CWork1st/Print.cpp
@@ -1,5 +1,7 @@
 #include "Print.h"
 #include <iomanip>
+#include <Windows.h>
+#include <conio.h>
 
 void Print(List* l)
 {
@@ -8,28 +10,55 @@ void Print(List* l)
 	system("cls");
 
 	List* temp = l;
+	int pcounter = 0;
+	int list = 0;
 
-	cout << " ________________________________________________________________________________" << endl;
-	cout << "| №  |   Код группы   |    Фамилия     | Год  | Часы : ув. / неув. причина | Пол |" << endl;
-	while (temp)
+	while (1)
 	{
-		cout
-			<< "| " << setw(2) << temp->p.id
-			<< " | " << setw(14)
-			<< temp->p.сodeGr << " | "
-			<< setw(14)
-			<< temp->p.surname << " | "
-			<< setw(4)
-			<< temp->p.year << " | "
-			<< setw(10)
-			<< temp->p.jHours << " | "
-			<< setw(13)
-			<< temp->p.sHours << " | "
-			<< setw(3) << temp->p.gender << " |" << endl;
-		temp = temp->next;
+		system("cls");
+		list++;
+		cout << " ________________________________________________________________________________" << endl;
+		cout << "| №  |   Код группы   |    Фамилия     | Год  | Часы : ув. / неув. причина | Пол |" << endl;
+		while (temp)
+		{
+			if (pcounter == 10)
+			{
+				cout << " ________________________________________________________________________________ " << endl;
+				cout << "Страница " << list << endl;
+				system("pause");
+				pcounter = 0;
+				char key = _getch();
+				if (GetAsyncKeyState(VK_RIGHT))
+				{
+					keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);//Отжимаем кнопку
+					system("cls");
+				}
+				break;
+			}
+			cout
+				<< "| " << setw(2) << temp->p.id
+				<< " | " << setw(14)
+				<< temp->p.сodeGr << " | "
+				<< setw(14)
+				<< temp->p.surname << " | "
+				<< setw(4)
+				<< temp->p.year << " | "
+				<< setw(10)
+				<< temp->p.jHours << " | "
+				<< setw(13)
+				<< temp->p.sHours << " | "
+				<< setw(3) << temp->p.gender << " |" << endl;
+			temp = temp->next;
+			pcounter++;
+		}
+		if (!temp)
+		{
+			cout << " ________________________________________________________________________________ " << endl;
+			cout << "Страница " << list << endl;
+			system("pause");
+			break;
+		}
 	}
-	cout << " ________________________________________________________________________________ " << endl;
-	system("pause");
 }
 void PrintBySur(List* l, string surname)
 {
