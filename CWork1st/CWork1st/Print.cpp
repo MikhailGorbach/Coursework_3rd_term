5465ebd0ff3b9c96e5d7e24619d362c9752e93bc
Bacha <michagorbach57@gmail.com>
2021-10-12 16:27:37 +0300

    new print

--- CWork1st/CWork1st/Print.cpp
+++ CWork1st/CWork1st/Print.cpp
@@ -19,47 +19,30 @@ void Print(List* l, int* id)
 	system("cls");
 
 	List* temp = l;
-	int pcounter = 0;
+	List* table = l;
 	int list = 0;
 	int endlist = ((*id) % 10 == 0) ? (*id) / 10 : ((*id) / 10) + 1;
-	List* table;
+	List* tempprev = l;
 
 	while (1)
 	{
-		system("cls");
-		list++;
-		cout << " ________________________________________________________________________________" << endl;
-		cout << "| №  |   Код группы   |    Фамилия     | Год  | Часы : ув. / неув. причина | Пол |" << endl;
-		while (temp)
+		char Key = _getch();
+		if (GetAsyncKeyState(VK_RIGHT))
 		{
-			
-			cout
-				<< "| " << setw(2) << temp->p.id
-				<< " | " << setw(14)
-				<< temp->p.сodeGr << " | "
-				<< setw(14)
-				<< temp->p.surname << " | "
-				<< setw(4)
-				<< temp->p.year << " | "
-				<< setw(10)
-				<< temp->p.jHours << " | "
-				<< setw(13)
-				<< temp->p.sHours << " | "
-				<< setw(3) << temp->p.gender << " |" << endl;
-			table = temp;
-			temp = temp->next;
-			pcounter++;
-			if (pcounter == 10 || table->p.id == (*id))
+			keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
+			if (list < endlist)
 			{
+				list++;
+				temp = PrintTen(temp);
 				cout << " ________________________________________________________________________________ " << endl;
 				cout << "Страница " << list << " из " << endlist << endl;
 				system("pause");
-				pcounter = 0;
-				char key = _getch();
-				break;
 			}
 		}
-		if (!temp) break;
+		if (GetAsyncKeyState(VK_DOWN))
+		{
+			break;
+		}
 	}
 }
 void PrintBySur(List* l, string surname)
@@ -162,4 +145,29 @@ void PrintAll(List* l, int* id)
 		system("pause");
 		break;
 	}
+}
+List* PrintTen(List* l)
+{
+	List* temp = l;
+
+	while (temp)
+	{
+		int pcounter = 0;
+		system("cls");
+		cout << " ________________________________________________________________________________" << endl;
+		cout << "| №  |   Код группы   |    Фамилия     | Год  | Часы : ув. / неув. причина | Пол |" << endl;
+		while (pcounter < 10)
+		{
+			cout
+				<< "| " << setw(2) << temp->p.id << " | " << setw(14)
+				<< temp->p.сodeGr << " | " << setw(14) << temp->p.surname << " | "
+				<< setw(4) << temp->p.year << " | " << setw(10) << temp->p.sHours << " | "
+				<< setw(13) << temp->p.jHours << " | "
+				<< setw(3) << temp->p.gender << " |" << endl;
+			temp = temp->next;
+			pcounter++;
+		}
+		break;
+	}
+	return temp;
 }
--- CWork1st/CWork1st/Print.h
+++ CWork1st/CWork1st/Print.h
@@ -5,3 +5,4 @@ void Print(List* l, int* id);								//Просмотр списка по 10 записей
 void PrintAll(List* l, int* id);							//Просмотр всего списка
 void PrintBySur(List* l, string surname);					//Просмотр списка студентов по фамилии
 int PrintById(List* l, int id);								//Просмотр списка по номеру
+List* PrintTen(List* l);
